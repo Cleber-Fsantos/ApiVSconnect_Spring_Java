8c9de45e32ffc0848b33f278cad3f6dd7e50fde0
Cleber-Fsantos <cleber.cfsantos@gmail.com>
2023-12-05 01:40:26 -0300

    Fixado Login

--- src/main/java/com/senai/apivsconnect/controllers/LoginController.java
+++ src/main/java/com/senai/apivsconnect/controllers/LoginController.java
@@ -13,8 +13,9 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
-
+@RestController
 public class LoginController {
     @Autowired
     private AuthenticationManager authenticationManager;
